2a9ca7a636513c0042dd7200d12e87ebdffac502
Miccah Castorina <m.castorina93@gmail.com>
2018-09-18 21:23:35 -0500

    Changed is_prime to a modified version of factors
    Moved increment array calculations into generate_wheel functions.
    These changes are for the Python version.

--- wheel_factorize.py
+++ wheel_factorize.py
@@ -10,18 +10,6 @@ def sieve_of_eratosthenes(n):
                 a[j] = 0
     return [i for i in range(2, len(a)) if a[i]]
 
-def is_prime(n):
-    # modified sieve that will quit early if multiple found
-    if n < 2: return False
-    n += 1
-    a = np.ones(n)
-    for i in range(2, int(np.ceil(np.sqrt(n)))):
-        if a[i]:
-            if (n-1) % i == 0: return False
-            for j in range(i*i, n, i):
-                a[j] = 0
-    return True
-
 def coprime(a, b):
     return np.gcd(a, b) == 1
 
@@ -42,11 +30,12 @@ class WheelFactor:
         for n in range(max(self.basis) + 1, np.prod(self.basis) + 2):
             if all(map(lambda y: coprime(y, n), self.basis)):
                 self.wheel.append(n)
+        inc = [self.wheel[i] - self.wheel[i-1] for i in range(1, len(self.wheel))]
+        inc += [self.wheel[0] + np.prod(self.basis) - self.wheel[-1]]
+        self.increments = inc
     def factors(self, n):
         if n <= 0: return None
         _factors = [1]
-        inc = [self.wheel[i] - self.wheel[i-1] for i in range(1, len(self.wheel))]
-        inc += [self.wheel[0] + np.prod(self.basis) - self.wheel[-1]]
         for v in self.basis:
             while n % v == 0:
                 _factors += [v]
@@ -58,7 +47,30 @@ class WheelFactor:
                 _factors += [k]
                 n //= k
             else:
-                k += inc[i]
-                i = (i+1) % len(inc)
+                k += self.increments[i]
+                i = (i+1) % len(self.increments)
         if n != 1: _factors += [n]
         return _factors
+    def is_prime(self, n):
+        # modified wheel that will quit early if 2 factors found
+        if n <= 0: return False
+        factor_count = 0
+        for v in self.basis:
+            while n % v == 0:
+                if factor_count >= 1: return False
+                factor_count += 1
+                n //= v
+        i = 0
+        k = self.wheel[0]
+        while k*k <= n:
+            if n % k == 0:
+                if factor_count >= 1: return False
+                factor_count += 1
+                n //= k
+            else:
+                k += self.increments[i]
+                i = (i+1) % len(self.increments)
+        if n != 1:
+            if factor_count >= 1: return False
+            factor_count += 1
+        return True
